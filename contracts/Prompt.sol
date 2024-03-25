cfede13e413ebfe532075a37223753894c7e42df
Ethan Chen <acuratnam@gmail.com>
2024-03-25 23:30:07 +0800

    mint2

--- contracts/Prompt.sol
+++ contracts/Prompt.sol
@@ -76,7 +76,9 @@ contract Prompt is AIOracleCallbackReceiver, ERC7007Opml {
         require(request.sender != address(0), "request not exists");
         request.output = output;
         prompts[request.modelId][string(request.input)] = string(output);
-        mint2(request.sender, request.input, "", requestId, string(output), "");
+        if (request.modelId == 50) {
+            mint2(request.sender, request.input, "", requestId, string(output), "");
+        }
         emit promptsUpdated(
             requestId,
             request.modelId,
